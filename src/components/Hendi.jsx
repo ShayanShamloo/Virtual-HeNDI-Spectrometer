c98668dc71cb19e344f750670575a33227bc92f8
Brennaser <sermanbc@dukes.jmu.edu>
2022-06-24 10:28:12 -0400

    Basic Swap

--- src/components/Hendi.jsx
+++ src/components/Hendi.jsx
@@ -4,20 +4,28 @@ import SVGComponent from "./SVGComponents";
 
 function Hendi({ id, seeOutside }) {
 
-  return (
-    <div className="hendi-box" id={id}> 
-      <SVGComponent style={{top:'0', stroke:'#fff0'}} className='click-through' part='laserexterior' /> 
-      <SVGComponent style={{top:'0'}} className='click-through' part='base' />
-      <SVGComponent style={{top:'0'}} className='click-through' part='lasersymbol' />
-      <SVGComponent style={{top:'0'}} className='click-through' part='rpump' />
-      <SVGComponent style={{top:'0'}} className='click-through' part='ddpump' />
-      <SVGComponent style={{top:'0'}} className='click-through' part='sdpump' />
-      <SVGComponent style={{top:'0'}} className='click-through' part='tpump' />
-      <SVGComponent style={{top:'0'}} className='click-through' part='sourcechamber' />
-      <SVGComponent style={{top:'0'}} className='click-through' part='dopingchamber' />
-      <SVGComponent style={{top:'0'}} className='click-through' part='detectionchamber'/>
-    </div>
-  );
+  if (seeOutside) {
+    return (
+      <div className="hendi-box" id={id}> 
+        <SVGComponent style={{top:'0', stroke:'#fff0'}} className='click-through' part='laserexterior' /> 
+        <SVGComponent style={{top:'0'}} className='click-through' part='base' />
+        <SVGComponent style={{top:'0'}} className='click-through' part='lasersymbol' />
+        <SVGComponent style={{top:'0'}} className='click-through' part='rpump' />
+        <SVGComponent style={{top:'0'}} className='click-through' part='ddpump' />
+        <SVGComponent style={{top:'0'}} className='click-through' part='sdpump' />
+        <SVGComponent style={{top:'0'}} className='click-through' part='tpump' />
+        <SVGComponent style={{top:'0'}} className='click-through' part='sourcechamber' />
+        <SVGComponent style={{top:'0'}} className='click-through' part='dopingchamber' />
+        <SVGComponent style={{top:'0'}} className='click-through' part='detectionchamber'/>
+      </div>
+    );
+  } else {
+    return (
+      <div className="hendi-box" id={id}>
+      <img src='./Virtual-HeNDI-Spectrometer/images/spinner.png' />
+      </div>
+    );
+  }
 }
 
 export default Hendi;
